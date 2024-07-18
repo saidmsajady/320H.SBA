61fd2298af44b5dba8eba8f2e9cfebdaf71f30a9
Said Masih <saidmsajady@gmail.com>
2024-07-18 13:56:34 -0700

    Set Up

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+
+  }, []);
+
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
